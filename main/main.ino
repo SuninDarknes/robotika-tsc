5194c5e2acad82c9a61dc52f12b67b4562540bd8
SuninDarknes <97896641+SuninDarknes@users.noreply.github.com>
2023-03-05 12:36:37 +0100

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,6 @@
+#include <AccelStepper.h>
 
-
-#define RIGHT_STEPPER_DIR 2
+#define RIGHT_STEPPER_DIR 11
 #define RIGHT_STEPPER_STEP 3
 #define LEFT_STEPPER_STEP 6
 #define LEFT_STEPPER_DIR 5
@@ -24,6 +24,8 @@ void setupPins() {
 
 
 
+AccelStepper LStepper(1, LEFT_STEPPER_STEP, LEFT_STEPPER_DIR);
+AccelStepper RStepper(1, RIGHT_STEPPER_STEP, RIGHT_STEPPER_DIR);
 class Steppers {
 public:
   static int setup() {
@@ -42,46 +44,131 @@ public:
   static void stop() {
     digitalWrite(6, LOW);
     digitalWrite(3, LOW);
+    ICR3 = 100;
+    ICR4 = 100;
   }
-  static void followLine() {
-    if (digitalRead(LEFT_IN_IR_SENSOR)) {
-      ICR3 = 10000;
-      ICR4 = 130;
-    }
+  static bool followLine() {
     if (digitalRead(RIGHT_IN_IR_SENSOR)) {
-      ICR3 = 130;
-      ICR4 = 10000;
+      ICR3 = 100;
+      ICR4 = 110;
+
+      return true;
     }
+    if (digitalRead(LEFT_IN_IR_SENSOR)) {
+      ICR3 = 110;
+      ICR4 = 100;
+      return true;
+    }
+    return false;
   }
 
-  static void followUntilTurn() {
-    do {
+  static void followUntilLeftTurn() {
+    Steppers::setup();
+
+    while (!digitalRead(LEFT_OUT_IR_SENSOR))
       followLine();
-    } while (!digitalRead(LEFT_OUT_IR_SENSOR) && !digitalRead(RIGHT_OUT_IR_SENSOR));
-    if (digitalRead(LEFT_OUT_IR_SENSOR)) {
-      ICR3 = 800;
-      ICR4 = 60;
-    } else {
-      ICR3 = 60;
-      ICR4 = 800;
+    Serial.println("Left line found!");
+
+    delay(600);
+
+    ICR3 = 5000;
+    delay(800);
+    while (!digitalRead(RIGHT_IN_IR_SENSOR)) {}
+    Steppers::stop();
+    delay(1000);
+  }
+  static void followUntilRightTurn() {
+    Steppers::setup();
+
+    while (!digitalRead(RIGHT_OUT_IR_SENSOR))
+      followLine();
+    delay(600);
+
+    ICR4 = 5000;
+    delay(800);
+    while (!digitalRead(LEFT_IN_IR_SENSOR)) {}
+    Steppers::stop();
+    delay(1000);
+  }
+  static void followUntilEnd() {
+    while (!(digitalRead(LEFT_IN_IR_SENSOR) && digitalRead(RIGHT_IN_IR_SENSOR)))
+      followLine();
+  }
+  static void Rotate(int degree) {
+    LStepper.setMaxSpeed(1000);
+    RStepper.setMaxSpeed(1000);
+
+    LStepper.setAcceleration(300);
+    RStepper.setAcceleration(300);
+    LStepper.setSpeed(1200);
+    RStepper.setSpeed(1200);
+    //3195 je pun krug
+    //3195/360 = 8.875
+    LStepper.move((int)(8.875 * degree));
+    RStepper.move((int)(8.875 * degree));
+    while (LStepper.distanceToGo() != 0 && RStepper.distanceToGo() != 0) {
+      LStepper.run();
+      RStepper.run();
     }
-    delay(500);
+    digitalWrite(RIGHT_STEPPER_DIR, LOW);
   }
 };
 
 
 void setup() {
   setupPins();
-  Steppers::setup();
-
+  delay(5000);
   ICR3 = 100;
   ICR4 = 100;
-
+  digitalWrite(RIGHT_STEPPER_DIR, LOW);
   Serial.begin(9600);
-}
 
-void loop() {
+  Steppers::followUntilLeftTurn();
+  Steppers::setup();
+  Steppers::followUntilEnd();
+  ICR3 = 100;
+  ICR4 = 100;
+  delay(2000);
+  Steppers::stop();
+  Steppers::Rotate(180);
+
+  Steppers::followUntilLeftTurn();
+  Steppers::followUntilRightTurn();
+  Steppers::setup();
+  Steppers::followUntilEnd();
+  ICR3 = 100;
+  ICR4 = 100;
+  delay(2000);
+  Steppers::stop();
+  Steppers::Rotate(180);
+
+  Steppers::followUntilRightTurn();
+  Steppers::followUntilLeftTurn();
   
-  while (true) {Steppers::followUntilTurn();
-  }
+  Steppers::setup();
+  Steppers::followUntilEnd();
+  ICR3 = 100;
+  ICR4 = 100;
+  delay(2000);
+  Steppers::stop();
+  Steppers::Rotate(180);
+
+  Steppers::followUntilLeftTurn();
+  Steppers::followUntilRightTurn();
+  Steppers::setup();
+  Steppers::followUntilEnd();
+  ICR3 = 100;
+  ICR4 = 100;
+  delay(2000);
+  Steppers::stop();
+  Steppers::Rotate(180);
+
+  Steppers::followUntilRightTurn();
+  Steppers::setup();
+  Steppers::followUntilEnd();
+
+
+  Steppers::stop();
+}
+void loop() {
 }
